6157b86042d13f8689200e6e65609ad39902ee01
Tobias Gierke <tobias.gierke@code-sourcery.de>
2013-06-05 01:46:37 +0200

    updated license headers

--- src/main/java/de/codesourcery/threadwatcher/ThreadEvent.java
+++ src/main/java/de/codesourcery/threadwatcher/ThreadEvent.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2012 Tobias Gierke <tobias.gierke@code-sourcery.de>
+ * Copyright 2013 Tobias Gierke <tobias.gierke@code-sourcery.de>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
